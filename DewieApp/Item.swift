c36493bcdb4c0175e2b54da0a3d365bbc171d6a8
hodgendazs <thomas.hodge89@gmail.com>
2024-05-16 21:10:54 -0600

    Initial Commit

---
+++ DewieApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DewieApp
+//
+//  Created by Thomas Hodge on 5/16/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
